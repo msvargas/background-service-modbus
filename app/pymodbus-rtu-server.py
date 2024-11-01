16a731eafb2e271aba9d0c12b0e553e446509b52
Michael Vargas <michael.vargas@mercadolibre.com.co>
2024-10-31 21:15:08 -0500

    Refactor Modbus RTU server to improve data fetching and configuration handling

--- app/pymodbus-rtu-server.py
+++ app/pymodbus-rtu-server.py
@@ -30,13 +30,13 @@ class CustomModbusDataBlock(ModbusSequentialDataBlock):
             data = await self.fetch_data_from_endpoint()
             if data:
                 self.update_values(data)
-            await asyncio.sleep(config["request_interval"])  # Espera de 1 minuto entre actualizaciones
+            await asyncio.sleep(60)  # Espera de 1 minuto entre actualizaciones
 
     async def fetch_data_from_endpoint(self):
         try:
             # URL del endpoint
             endpoint_url = config["endpoint_url"]
-            response = requests.get(endpoint_url, timeout=60)
+            response = requests.get(endpoint_url)
             response.raise_for_status()
             return response.json()
         except requests.exceptions.RequestException as e:
@@ -49,17 +49,25 @@ class CustomModbusDataBlock(ModbusSequentialDataBlock):
                 measure_type = obj.get("measure_type", "desconocido")  # Si no hay "measure_type", usa "desconocido"
                 log.debug(f"Actualizando valor de {measure_type} ID:{obj['id']} => {obj['value']}")
                 # Multiplicar el valor por 10 para simular un valor de 1 decimal y enviar un entero siempre
-                value = int(obj["value"] * config["multiplier"])
+                value = int(obj["value"] * 10)
                 self.setValues(obj["id"], [value])
             else:
                 measure_type = obj.get("measure_type", "desconocido")  # Si no hay "measure_type", usa "desconocido"
                 log.warning(f"Datos inválidos para {measure_type}: {obj}")
 
+    def getValues(self, address, count=1):
+        data = super().getValues(address, count)
+        log.debug(f"Obteniendo valores desde la dirección {address} hasta {address+count-1}")
+        log.debug(f"Valores obtenidos: {data}")
+        # send fixed 10 number from 80 to 89
+        #      data = [10] * count
+        #      return data
+        return data
+datablock = CustomModbusDataBlock(0x000, [0]*100)
 # Crear el contexto Modbus con la clase personalizada
 store = ModbusSlaveContext(
-    hr=CustomModbusDataBlock(0x000, [0]*100),  # Assume max 6 registers for initialization
+    hr= datablock,  # Assume max 6 registers for initialization
 )
-context = ModbusServerContext(slaves=store, single=True)
 
 identity = ModbusDeviceIdentification()
 identity.VendorName = 'ModbusServer'
@@ -70,20 +78,26 @@ identity.ModelName = 'Modbus RTU Server Model'
 identity.MajorMinorRevision = '1.0'
 
 if __name__ == "__main__":
-    required_keys = ["endpoint_url", "serial_port", "baudrate", "timeout", "stopbits", "bytesize", "parity", "slave_id", "request_interval", "multiplier"]
+    required_keys = ["endpoint_url", "serial_port", "baudrate", "timeout", "stopbits", "bytesize", "parity", "slave_id"]
     for key in required_keys:
         if key not in config:
             raise ValueError(f"The {key} is required in the config.json file")
-    
-    StartSerialServer(
-        context=context,
-        identity=identity,
-        port=config["serial_port"],
-        baudrate=config["baudrate"],
-        timeout=config["timeout"],
-        stopbits=config["stopbits"],
-        bytesize=config["bytesize"],
-        parity=config["parity"],
-        id=config["slave_id"],
-        method='rtu',
-    )
+    context = ModbusServerContext(slaves={ config["slave_id"]: store}, single=False)
+    # Configuration for StartSerialServer
+    server_kwargs = {
+        "context": context,
+        "identity": identity,
+        "port": config["serial_port"],
+        "baudrate": config["baudrate"],
+        "timeout": config["timeout"],
+        "stopbits": config["stopbits"],
+        "bytesize": config["bytesize"],
+        "parity": config["parity"],
+        "method": 'rtu'
+    }
+
+    # Start the server in a separate thread to avoid blocking the asyncio loop
+    loop = asyncio.get_event_loop()
+    loop.run_in_executor(None, lambda: StartSerialServer(**server_kwargs))
+    loop.run_forever()
+
